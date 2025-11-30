c7eafb7c687e5a9657b9bd55f9b483ed6e649595
Je suis bleu <93595424+LIPMO@users.noreply.github.com>
2025-11-30 23:09:07 +0100

    Update Mapbox access token and improve functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-mapboxgl.accessToken = 'REMPLACE_PAR_TA_CLE_MAPBOX'
-const socket = io()
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxlc2JhZXJ0IiwiYSI6ImNtYzY2Y2Q1YTBibG4yanM2c29oYnRjczYifQ.ldeqUPrH69_SpOtiF9uo-w'
+const socket = io(window.location.origin)
 
 const login = document.getElementById("login")
 const hud = document.getElementById("hud")
@@ -7,11 +7,14 @@ const speedSpan = document.getElementById("speed")
 const vmaxSpan = document.getElementById("vmax")
 
 let map, marker
-let vmax = localStorage.getItem("vmax") || 0
+let lastPos = null
+let vmax = Number(localStorage.getItem("vmax")) || 0
+
+const MAX_DISPLAY_SPEED = 120
+const MAX_DISTANCE_KM = 5
 
 function start() {
-  let pseudo = document.getElementById("pseudoInput").value
-
+  let pseudo = document.getElementById("pseudoInput").value || localStorage.getItem("pseudo")
   if (!pseudo) return alert("Pseudo obligatoire")
 
   localStorage.setItem("pseudo", pseudo)
@@ -27,14 +30,16 @@ function initMap(){
   map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v11',
-    zoom: 15
+    zoom: 16
   })
 
   navigator.geolocation.watchPosition(pos => {
-
     const lat = pos.coords.latitude
     const lon = pos.coords.longitude
     let speed = pos.coords.speed ? Math.round(pos.coords.speed * 3.6) : 0
+
+    if (speed > 200) speed = 0
+    if (speed > MAX_DISPLAY_SPEED) speed = MAX_DISPLAY_SPEED
 
     if (speed > vmax){
       vmax = speed
@@ -46,46 +51,95 @@ function initMap(){
 
     if (!marker){
       marker = new mapboxgl.Marker({color:'deepskyblue'})
-      .setLngLat([lon, lat])
-      .addTo(map)
+        .setLngLat([lon, lat])
+        .addTo(map)
       map.setCenter([lon, lat])
     } else {
       marker.setLngLat([lon, lat])
     }
 
-    socket.emit("update", {
-      lat, lon, speed, vmax
-    })
+    lastPos = { lat, lon }
 
+    socket.emit("update", { lat, lon, speed, vmax })
   },
-  () => alert("GPS OFF"),
+  () => alert("GPS désactivé"),
   { enableHighAccuracy: true })
 }
 
-// Transmission au serveur
-socket.emit("join", {
-  pseudo: localStorage.getItem("pseudo"),
-  vmax
-})
+socket.emit("join", { pseudo: localStorage.getItem("pseudo"), vmax })
 
-// Affichage des autres
+/* ---------------- AUTRES RIDERS ----------------- */
 const riders = {}
 
 socket.on("users", data => {
+  if (!map || !lastPos) return
+
+  Object.keys(riders).forEach(id => {
+    if (!data[id]) {
+      riders[id].marker.remove()
+      riders[id].card.remove()
+      delete riders[id]
+    }
+  })
 
   Object.keys(data).forEach(id => {
+    if (!data[id].lat || !data[id].lon) return
 
-    if (!data[id].lat) return
+    const dist = getDistance(lastPos.lat, lastPos.lon, data[id].lat, data[id].lon)
+    if (dist > MAX_DISTANCE_KM) {
+      if (riders[id]) {
+        riders[id].marker.remove()
+        riders[id].card.remove()
+        delete riders[id]
+      }
+      return
+    }
+
+    const cardContent = `
+      <div class="user-card">
+        <b>${data[id].pseudo}</b><br>
+        ${data[id].speed} km/h<br>
+        Vmax ${data[id].vmax}
+      </div>
+    `
 
     if (!riders[id]) {
-      riders[id] = new mapboxgl.Marker({color:'orange'})
-      .setLngLat([data[id].lon, data[id].lat])
-      .setPopup(new mapboxgl.Popup().setHTML(
-        `<b>${data[id].pseudo}</b><br>Vitesse: ${data[id].speed}<br>Vmax: ${data[id].vmax}`
-      ))
-      .addTo(map)
+      const el = document.createElement('div')
+      el.className = 'user-dot'
+
+      const card = new mapboxgl.Marker({ offset: [0,-40] })
+        .setLngLat([data[id].lon, data[id].lat])
+        .setPopup(new mapboxgl.Popup({ closeButton:false, closeOnClick:false }).setHTML(cardContent))
+        .addTo(map)
+
+      card.togglePopup()
+
+      riders[id] = {
+        marker: new mapboxgl.Marker(el)
+          .setLngLat([data[id].lon, data[id].lat])
+          .addTo(map),
+        card
+      }
     } else {
-      riders[id].setLngLat([data[id].lon, data[id].lat])
+      riders[id].marker.setLngLat([data[id].lon, data[id].lat])
+      riders[id].card.setLngLat([data[id].lon, data[id].lat])
+        .getPopup().setHTML(cardContent)
     }
   })
 })
+
+/* ---------------- DISTANCE KM ----------------- */
+function getDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371
+  const dLat = (lat2 - lat1) * Math.PI / 180
+  const dLon = (lon2 - lon1) * Math.PI / 180
+
+  const a =
+    Math.sin(dLat/2)**2 +
+    Math.cos(lat1 * Math.PI / 180) *
+    Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2)**2
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+  return R * c
+}
